238cd7121d318ea1e4c58024f2aac2f47f3f20a3
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2026-02-10 16:48:12 +0200

    refactor: removed macOS mention and listed iOS 18.0+,.

--- Package.swift
+++ Package.swift
@@ -7,8 +7,7 @@ import PackageDescription
 let package = Package(
     name: "UIComponents",
     platforms: [
-        .iOS(.v18),
-        .macOS(.v15)
+        .iOS(.v18)
     ],
     products: [
         .library(
